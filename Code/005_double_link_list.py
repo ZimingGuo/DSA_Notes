6194abed781708c6b957c37afb822afed67cd9ad
ZimingGuo <guoziming99999@icloud.com>
2020-08-30 12:31:42 +0800

    双向链表over debug了半个小时...

--- Code/005_double_link_list.py
+++ Code/005_double_link_list.py
@@ -1,6 +1,7 @@
 # author: Ziming Guo
 # time: 2020/8/30
 
+
 class Node(object):
     """定义结点"""
 
@@ -54,8 +55,8 @@ class DoubleLinkList(object):
             while cur.next is not None:
                 # 找到最后一个结点
                 cur = cur.next
-                cur.next = node
-                node.prev = cur
+            cur.next = node
+            node.prev = cur
 
     def insert(self, pos, item):
         """
@@ -67,19 +68,74 @@ class DoubleLinkList(object):
         elif pos > (self.length() - 1):
             self.append(item)
         else:  # 找到指定位置进行插入
-            node=Node(item)
+            cur = self.__head
+            count = 0
+            while count < pos:
+                count += 1
+                cur = cur.next
+            node = Node(item)
             node.next = cur
             node.prev = cur.prev
             cur.prev.next = node
             cur.prev = node
 
-
     def remove(self, item):
         """删除节点"""
+        cur = self.__head
+        while cur is not None:
+            if cur.elem == item:
+                # 找到了所要删除的元素所在结点
+                if cur == self.__head:  # 如果所要删除的结点就是第一个结点
+                    self.__head = cur.next
+                    if cur.next is not None:  # 判断链表是否只有一个结点
+                        cur.next.prev = None
+                else:
+                    cur.prev.next = cur.next
+                    if cur.next:  # 判断是否是最后一个结点
+                        cur.next.prev = cur.prev
+                break
+            else:
+                cur = cur.next  # 继续向下移动
 
-    def search(item):
+    def search(self, item):
         """查找节点是否存在"""
+        cur = self.__head
+        while cur is not None:
+            if cur.elem == item:
+                return True
+            else:
+                cur = cur.next
+            return False
 
 
 if __name__ == "__main__":
-    dll = DoubleLinkList()
+    ll = DoubleLinkList()
+    print(ll.is_empty())
+    print(ll.length())
+
+    ll.append(99)
+    print(ll.is_empty())
+    print(ll.length())
+    ll.travel()
+    print("*******************************")
+
+    ll.append(2)
+    ll.add(8)
+    ll.travel()
+    
+    print("*******************************")
+    ll.append(3)
+    ll.append(4)
+    ll.append(5)
+    ll.append(6)
+    # 8 1 2 3 4 5 6
+
+    ll.insert(-2, 9)
+    ll.travel()
+    # 9 8 1 2 3 4 5 6
+
+    ll.travel()
+    ll.remove(9)
+    ll.remove(6)
+
+    ll.travel()
